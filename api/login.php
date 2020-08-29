d689827600b2875df52ec42157cd83bc22516af3
santiazap943 <sazap@correo.udistrital.edu.co>
2020-08-29 10:25:09 -0500

    ajustes

--- api/login.php
+++ api/login.php
@@ -43,6 +43,6 @@ if($administrator -> logIn($email, $password)){
         http_response_code(401);
         $administrator = new Administrator();
         $administrator ->select();
-        echo json_encode(array("correo" => $administrator -> getEmail(), "id" => $administrator -> getIdAdministrator()));
+        //echo json_encode(array("correo" => $administrator -> getEmail(), "id" => $administrator -> getIdAdministrator()));
         echo json_encode(array("message" => "Login failed.", "password" => $password));
     }
